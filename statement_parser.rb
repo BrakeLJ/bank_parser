9135ad3278f86e50d1d8138f90f87b2e826384b5
Lucas Brake <lukejbrake@gmail.com>
2025-01-30 19:54:05 -0700

    feat: add analyze_statement with the categorize method still to impliment

--- statement_parser.rb
+++ statement_parser.rb
@@ -6,5 +6,25 @@ CATEGORIES = {
   'Entertainment' => ['netflix', 'spotify', 'theater', 'cinema', 'concert'],
   'Utilities' => ['hydro', 'electric', 'internet', 'bell', 'rogers'],
   'Transport' => ['uber', 'lyft', 'gas', 'shell', 'esso'],
-  'Miscellaneous' => []
+  'Uncategorized' => []
 }
+
+def categorize
+  # Categorize transactions based on the description and the mapping of keywords in CATEGORIES. 
+  # Anything that cannot be found should be placed in Uncategorized
+end
+
+def analyze_statement(file)
+  transactions = []
+
+  CSV.foreach(file, headers: true) do |row| 
+    date = row[:date]
+    description = row[:description]
+    amount = row[:amount]
+    category = 
+
+    transactions << {date: date, description: description, amount: amount}
+
+    # Call a method to output a report to the console.
+  end
+end
