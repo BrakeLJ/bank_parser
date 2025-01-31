69afeb6997227c6b1684d0bc53901e786c3824ce
Lucas Brake <lukejbrake@gmail.com>
2025-01-31 09:13:55 -0700

    feat: add categorize method logic with note to make the method more robust in the future

--- statement_parser.rb
+++ statement_parser.rb
@@ -1,4 +1,4 @@
-
+require 'csv'
 
 CATEGORIES = {
   'Groceries' => ['walmart', 'costco', 'superstore', 'whole foods', 'grocery'],
@@ -9,22 +9,32 @@ CATEGORIES = {
   'Uncategorized' => []
 }
 
-def categorize
-  # Categorize transactions based on the description and the mapping of keywords in CATEGORIES. 
-  # Anything that cannot be found should be placed in Uncategorized
+def categorize(description)
+  puts description
+  description = description.downcase
+
+  # If the description matches any of the category mapping keywords return that category, otherwise return Uncategorized.
+  # TODO What if the description matches multiple categories?
+  CATEGORIES.each do |category, keywords|
+    return category if keywords.any? { |word| description.include?(word)}
+  end
+  'Uncategorized'
 end
 
 def analyze_statement(file)
   transactions = []
 
   CSV.foreach(file, headers: true) do |row| 
-    date = row[:date]
-    description = row[:description]
-    amount = row[:amount]
-    category = 
+    date = row['Date']
+    description = row['Description']
+    amount = row['Amount'].to_f
+    category = categorize(description)
+    puts row['date']
 
     transactions << {date: date, description: description, amount: amount}
 
     # Call a method to output a report to the console.
   end
 end
+
+analyze_statement('statement.csv')
