840f9865005018536b91644a6202bcc8d5b95f9b
Lucas Brake <lukejbrake@gmail.com>
2025-01-30 19:33:41 -0700

    chore: project setup

---
+++ statement_parser.rb
@@ -0,0 +1,10 @@
+
+
+CATEGORIES = {
+  'Groceries' => ['walmart', 'costco', 'superstore', 'whole foods', 'grocery'],
+  'Dining' => ['restaurant', 'mcdonald', 'starbucks', 'dining', 'cafe'],
+  'Entertainment' => ['netflix', 'spotify', 'theater', 'cinema', 'concert'],
+  'Utilities' => ['hydro', 'electric', 'internet', 'bell', 'rogers'],
+  'Transport' => ['uber', 'lyft', 'gas', 'shell', 'esso'],
+  'Miscellaneous' => []
+}
